242d2ec8103a56ba7c377ce8dcd9ef940effe84d
Mashiro-jz <a.wiacek@zlotoryja.xyz>
2026-02-03 08:58:34 +0100

    Implemented dataSources && repository implementention

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,56 @@
+import 'package:agym/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:agym/features/auth/data/models/user_model.dart';
+import 'package:agym/features/auth/domain/entities/user.dart';
+import 'package:agym/features/auth/domain/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepositoryImpl({required this.authRemoteDataSource});
+
+  @override
+  Future<User?> getCurrentUser() async {
+    final userModel = await authRemoteDataSource.getCurrentUser();
+    return userModel?.toEntity();
+  }
+
+  @override
+  Future<User> login({required String email, required String password}) async {
+    final userModel = await authRemoteDataSource.login(
+      email: email,
+      password: password,
+    );
+    return userModel.toEntity();
+  }
+
+  @override
+  Future<void> logout() async {
+    return await authRemoteDataSource.logout();
+  }
+
+  @override
+  Future<User> register({
+    required String email,
+    required String password,
+    required User user,
+  }) async {
+    final userModel = UserModel(
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      email: email,
+      photoUrl: user.photoUrl,
+      sexRole: user.sexRole,
+      userRole: user.userRole,
+    );
+
+    final registeredModel = await authRemoteDataSource.register(
+      email: email,
+      password: password,
+      user: userModel,
+    );
+
+    return registeredModel.toEntity();
+  }
+}
