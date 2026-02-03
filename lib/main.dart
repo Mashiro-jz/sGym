f255f353e3d2e55d687c41955f042908626896bc
Mashiro-jz <a.wiacek@zlotoryja.xyz>
2026-02-03 11:55:18 +0100

    Implemented firebase config and injection_container

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'core/config/injection_container.dart' as di;
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await di.init();
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'aGym',
+      home: Scaffold(body: Center(child: Text("Witaj w aGym!"))),
     );
   }
 }
